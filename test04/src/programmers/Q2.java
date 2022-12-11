00010bef9f1a76d3383768f81ba606c8b594282b
myeonghyeonKim0922 <cdeo7788@naver.com>
2022-12-11 17:16:26 +0900

    UP

---
+++ test04/src/programmers/Q2.java
@@ -0,0 +1,14 @@
+package programmers;
+import java.util.*;
+
+public class Q2 {
+
+	class Solution {
+		    public int[] solution(int[] numbers, int num1, int num2) {
+		    	num2 = num2 +1;
+		        int[] answer = Arrays.copyOfRange(numbers, num1, num2);
+		        return answer;
+		    }
+		}
+
+}

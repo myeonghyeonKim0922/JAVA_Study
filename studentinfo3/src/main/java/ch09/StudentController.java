bb30f2d2abde8f7f5975bd412209fc92dcda4269
github myeonghyeonKim0922echo <cdeo7788@naver.com>
2022-12-16 18:11:37 +0900

    up

---
+++ studentinfo3/src/main/java/ch09/StudentController.java
@@ -0,0 +1,75 @@
+package ch09;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+/**
+ * Servlet implementation class StudentController
+ */
+//StudentController s = new StudentController(); 서블릿 객체 생성은 톰캣에서 해준다
+@WebServlet("/studentControl")
+public class StudentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	StudentDAO dao;
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config); //서블릿 초기화
+		dao = new StudentDAO(); // StudentDAO 객체가 딱 한번만 만들어진다. -> 공유해서 쓸수 있다.
+	}
+	
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//request : 뷰에서 넘어오는 데이터, 정보가 들어있다.
+		//뷰에서 넘어오는 한글 깨짐 방지
+		request.setCharacterEncoding("utf-8");
+		String action = request.getParameter("action"); //insert
+		String view = "";
+		
+		view = insert(request,response); //request,response객체를 매개변수로 넘겨준다.
+		
+		//getServletContext(): ServletContext를 얻어옴
+		//getRequestDispatcher(이동할 페이지): 이동할 페이지의 경로 지정
+		//forward: 페이지를 이동시킨다. 내부에서 이동이 되므로 주소값은 그대로
+		getServletContext().getRequestDispatcher("/ch09/"+view)
+		.forward(request, response);
+	}
+	
+	//request 데이터 받아옴 -> DAO에 있는 insert 실행(DB에 insert가 됨) -> 페이지명 리턴
+    public String insert(HttpServletRequest request, HttpServletResponse response)   {
+    	Student s = new Student();
+    	try {
+			BeanUtils.populate(s, request.getParameterMap());
+			
+			
+			
+			/*
+			BeanUtils.populate(s, request.getParameterMap()); -> 아래 코드의 역할을 대신 해준다.
+			
+			
+    	s.setUsername(request.getParameter("username"));
+    	s.setEmail(request.getParameter("email"));
+    	s.setUniv(request.getParameter("univ"));
+    	s.setBirth(request.getParameter("birth"));
+			 */
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	
+    	
+    	dao.insert(s); //컨트롤러는 DAO한테 있는 메소드를 사용한다. DAO한테 데이터 베이스관련 요청을 해야한다.
+    	return "studentinfo.jsp";
+    }
+ 
+}
---
+++ studentinfo3/src/main/java/ch09/StudentDAO.java
@@ -0,0 +1,86 @@
+package ch09;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class StudentDAO {
+	Connection conn = null; //데이터베이스랑 연결을 담당.
+	PreparedStatement pstmt; //쿼리문의 실행을 담당한다.
+	
+	//jdbc 뜻 : 자바하고 DB를 연결해주는 api -> ojbcd6.jar
+	final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+	final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	
+	//DB연결 메소드
+	public void open() {
+		try {
+			Class.forName(JDBC_DRIVER); //드라이브 로드
+			conn = DriverManager.getConnection(JDBC_URL,"test","test1234"); //DB연결
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	//DB닫는 메소드
+	public void close() {
+		try {
+			//★pstmt,conn은 리소스(데이터를 읽고 쓰는 객체) 이므로 사용 후 반드시 닫아줘야 한다.
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//학생 정보를 다 불러온다.
+	public ArrayList<Student> getAll(){
+		open();//DB오픈
+		ArrayList<Student> students = new ArrayList<>(); //student객체를 담을 리스트 준비.
+		try {
+			pstmt = conn.prepareStatement("SELECT * FROM STUDENT");//실행할 쿼리문을 입력한다.
+			ResultSet rs = pstmt.executeQuery(); //쿼리문 실행(select문 사용시 사용) //ResertSet: 데이터를 받는 역할을 한다
+			
+			while(rs.next()) { //한 행씩 값이 있는지 없는지 판단한다.
+				Student s = new Student();
+				s.setId(rs.getInt("id"));
+				s.setUsername(rs.getString("username"));
+				s.setUniv(rs.getString("univ"));
+				s.setBirth(rs.getDate("birth"));
+				s.setEmail(rs.getString("email"));
+				
+				students.add(s);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return students;
+	}
+	//학생 정보를 입력.
+	public void insert(Student s) {
+		open();
+		//물음표: 어떤 데이터가 들어올지 모른다.
+		String sql = "insert into student values(id_seq.nextval,?,?,?,?)";
+		
+			try {
+				pstmt = conn.prepareStatement(sql);//실행할 쿼리문 입력
+				//오라클의 데이터 타입으로 변환을 해준다.
+				pstmt.setString(1, s.getUsername()); //pstmt.setString(값을 넣어줄 위치,넣어줄 데이터);
+				pstmt.setString(2, s.getUniv());
+				pstmt.setDate(3, s.getBirth());
+				pstmt.setString(4, s.getEmail());
+				
+				pstmt.executeUpdate(); //insert,delete,update 실행시
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				close();
+			}
+	}
+	
+}
